2a3078eb48d7e8560ded0249fb29c012f5d84460
Juan Urbiñez Gómez <juan.urbinez1@udea.edu.co>
2021-09-25 17:47:02 -0500

    Avance
    mas mapas

--- Codigo/Parcial_2/main.cpp
+++ Codigo/Parcial_2/main.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 int main()
 {
-    int ultimo;
-
+    float A=0;
     int c=0; int AlturaInicial,AnchoInicial;
     unsigned long long rojo,verde,azul;
-    string filname = "../Parcial_2/Mapas/Colombia 6x4.png"; //cargar la imagen
+    string filname = "../Parcial_2/Mapas/Colombia 4x8.png"; //cargar la imagen
                     //retrocede/ingresa a la carpeta/nombre de la imagen o /nombre de la carp. imagen/nom. imagen
     QImage im(filname.c_str());
 
 
-    cout<<im.width()<<endl;
-    cout<<im.width()<<endl;
-    cout<<im.height()<<endl;
-    cout<<im.height()<<endl;
+    //cout<<im.width()<<endl;
+    //cout<<im.width()<<endl;
+    //cout<<im.height()<<endl;
+    //cout<<im.height()<<endl;
 if(im.height()%16!=0)
 {
 
@@ -24,6 +23,15 @@ if(im.height()%16!=0)
 AlturaInicial=im.height();
 AnchoInicial=im.width();
 
+cout<<im.height()<<endl;
+
+A=(16%im.height());
+
+cout<<A<<endl;
+
+//A=(16%im.height())*im.height();
+
+//cout<<A<<endl;
 
 //Sobremuestreo
 for(int indey=0;indey<AlturaInicial;indey+=1)
@@ -32,11 +40,11 @@ for(int indey=0;indey<AlturaInicial;indey+=1)
     {
         for(int clock=0;clock<4;clock+=1)
         {
-        rojo=im.pixelColor(index,indey).red();
-        verde=im.pixelColor(index,indey).green();
-        azul=im.pixelColor(index,indey).blue();
-        c++;
-        cout <<index<<", "<<indey<<" = "<< "rojo: "<<rojo<<'\t'<<"verde: "<<verde<<'\t'<<"azul: "<<azul<<endl;
+            rojo=im.pixelColor(index,indey).red();
+            verde=im.pixelColor(index,indey).green();
+            azul=im.pixelColor(index,indey).blue();
+            c++;
+            cout <<index<<", "<<indey<<" = "<< "rojo: "<<rojo<<'\t'<<"verde: "<<verde<<'\t'<<"azul: "<<azul<<endl;
         }
         //cout<<c<<endl;
     }
@@ -46,6 +54,14 @@ ofstream archivo;
 c=0;
 archivo.open(nombreArchivo.c_str(), fstream::out);
 archivo<<"{"<<endl;
+
+//pixeles faltantes
+
+
+
+
+
+
 
 for(int indey=0;indey<AlturaInicial;indey+=1)
 {
